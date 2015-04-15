e6a64167f9ac0fd507efac51cf55b381f3d28437
rafalo1333 <rafal.kaczor.1993@gmail.com>
2015-04-15 11:14:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#: -*- encoding: utf-8 -*-
+
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+
+class ${PROJECT_NAME}App(App):
+    pass
+
+if __name__ == '__main__':
+    ${PROJECT_NAME}App().run()
